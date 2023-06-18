85e275348970d6aedd8ae81906dc5aabf2f4b15e
choiseongyun <5songyon5@naver.com>
2023-06-18 16:59:24 +0900

    error update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,10 +45,9 @@ const Home: React.FC = () => {
   const loading = useSelector((state: RootState) => state.exchange.loading);
 
   // 선택된 거래소와 관련된 상태를 관리하기 위해 useState 훅을 사용합니다.
-  // const [selectedExchange, setSelectedExchange] = useState<Exchange | null>(null);
   const [pairModalVisible, setPairModalVisible] = useState(false);
   const [supportedPairs, setSupportedPairs] = useState<string[] | undefined>(undefined);
-  //const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc'); // 정렬 방식 상태 변수 추가
+
 
   // useEffect 훅을 사용하여 컴포넌트가 마운트되었을 시 fetchExchanges 액션을 디스패치합니다.
   useEffect(() => {
@@ -62,7 +61,7 @@ const Home: React.FC = () => {
    */
   const handleRowClick = async (exchange: Exchange) => {
     console.log('Clicked Row:', exchange);
-    // setSelectedExchange(exchange);
+
     setPairModalVisible(true);
     try {
       const pairs = await fetchSupportedPairs(exchange.id) as string[];
@@ -77,40 +76,10 @@ const Home: React.FC = () => {
    * 모달을 닫고 선택된 거래소와 관련된 상태를 초기화하는 함수입니다.
    */
   const handleModalClose = () => {
-    // setSelectedExchange(null);
+
     setPairModalVisible(false);
   };
-  
-  /**
-   * @author 최성윤
-   * 오름차순으로 정렬을 처리하는 이벤트 핸들러 함수입니다.
-   */
-  // const handleSortAsc = () => {
-  //   setSortOrder('asc');
-  // };
-  
-  /**
-   * @author 최성윤
-   * 내림차순으로 정렬을 처리하는 이벤트 핸들러 함수입니다.
-   */
-  // const handleSortDesc = () => {
-  //   setSortOrder('desc');
-  // };
-  
-  /**
-   * 거래소를 정렬된 상태로 반환하는 함수입니다.
-   * @author 최성윤
-   * @param a 첫 번째 거래소
-   * @param b 두 번째 거래소
-   * @returns 정렬된 거래소 배열
-   */
-  // const sortedExchanges = [...exchanges].sort((a, b) => {
-  //   if (sortOrder === 'asc') {
-  //     return a.trade_volume_24h_btc - b.trade_volume_24h_btc;
-  //   } else {
-  //     return b.trade_volume_24h_btc - a.trade_volume_24h_btc;
-  //   }
-  // });
+
   /**
    * 지원되는 페어를 가져오는 비동기 함수입니다.
    * @author 최성윤
